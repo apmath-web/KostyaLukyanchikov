ee01beac0200f3332032be8a8fdfa0feec709f66
Kostya Lukyanchikov <mrwithoutnickname@gmail.com>
2018-10-02 22:21:05 +0300

    server upd

--- server.go
+++ server.go
@@ -1,17 +1,31 @@
 package main
+
 import (
+	"github.com/apmath-web/KostyaLukyanchikov/tree/dev/handlers"
+	"fmt"
 	"net/http"
-	"strings"
 )
-func sayHello(w http.ResponseWriter, r *http.Request) {
-	message := r.URL.Path
-	message = strings.TrimPrefix(message, "/")
-	message = "Hello " + message
-	w.Write([]byte(message))
+
+func rootpage(w http.ResponseWriter, r *http.Request)  {
+	fmt.Fprint(w, "Hello World!")
 }
-func main() {
-	http.HandleFunc("/", sayHello)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		panic(err)
-	}
+
+//func date(w http.ResponseWriter, r *http.Request) {
+//	http.HandleFunc("/date",
+//	fmt.Fprintln(w, "Date:", time.Now().UTC().Format("2006-01-02T15:04:05-0700"))
+//})
+func main()  {
+
+	//http.HandleFunc("/wait",
+	//	func(w http.ResponseWriter, r *http.Request) {
+	//		fmt.Fprintln(w, "Date:", time.Now().UTC().Format("2006-01-02T15:04:05-0700"))
+	//	})
+
+
+	http.HandleFunc("/", rootpage)
+	http.HandleFunc("/date", handlers.Date)
+
+
+	fmt.Println("Starting server at :8080")
+	http.ListenAndServe(":8080", nil)
 }
