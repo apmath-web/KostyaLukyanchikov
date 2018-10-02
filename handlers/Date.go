a69efeb274f0440c1648076c1e135a928a48c48f
Kostya Lukyanchikov <mrwithoutnickname@gmail.com>
2018-10-02 22:16:10 +0300

    Handler for date

---
+++ handlers/Date.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func date(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Date:", time.Now().UTC().Format("2006-01-02T15:04:05-0700"))
+}
